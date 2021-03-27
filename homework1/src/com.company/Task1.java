130433aa59a9536ebb0412415b053637da6dd4ed
Dmitriy <trueprogramdevelop@gmail.com>
2021-03-27 17:13:10 +0300

    add homework1

---
+++ homework1/src/com.company/Task1.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task1 {
+
+    public static void task1(BufferedReader reader) {
+        try {
+            System.out.println("Введите радиус окружности в сантиметрах:");
+            double r = Double.parseDouble(reader.readLine());
+            System.out.println("Площадь круга: " + String.format("%.2f", (Math.PI * Math.pow(r, 2))) + " см.кв.");
+            System.out.println("Длина окружности: " + String.format("%.2f", (2 * Math.PI * r)) + " см.");
+        } catch (IOException ex) {
+            // pass
+        }
+    }
+}
