aa80ad7533c5d4ef8b133a3f9533887295375ab4
Dmitriy <trueprogramdevelop@gmail.com>
2021-04-20 14:23:27 +0300

    Add ant to replit and build project

--- run_local.sh
+++ run_local.sh
@@ -1,31 +1,11 @@
 #!/bin/bash
 
-url=https://get.sdkman.io
-script_path="/home/runner/.sdkman/bin/sdkman-init.sh"
-
-eval "curl -s ${url} | bash"
-
-wait
-
-eval "cd /home/runner/.sdkman/bin/ && . ./sdkman-init.sh && cd && cd ./javahomeworks"
-
-wait
-
-eval "sdk install ant"
-
-wait
-
 eval "ant compile"
-
-wait
-
 eval "clear"
 
 echo "homework №1 - basics"
-
 commando1="java -classpath result/homework_1 com.dromakin.starter.Starter"
 echo "homework №2 - generics"
-
 commando2="java -classpath result/homework_2 com.dromakin.starter.Starter"
 echo "Enter number of project : "
 
